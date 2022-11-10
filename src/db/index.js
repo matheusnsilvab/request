0646aea0a344f2f59a8e81465d99dcc68ccba61e
Matheus Nunes <103690968+matheusnsilvab@users.noreply.github.com>
2022-11-10 14:00:28 -0300

    Add files via upload

---
+++ src/db/index.js
@@ -0,0 +1,20 @@
+module.exports = {
+    usuarios: [
+        {
+            id: 1,
+            nome: "Matheus",
+            idade: 23,
+            perfil_id: 1,
+        },
+        {
+            id: 2,
+            nome: "Douglas",
+            idade: 22,
+            perfil_id: 2,
+        },
+    ],
+    perfis: [
+        { id: 1, descricao: "ADMIN" },
+        { id: 2, descricao: "NORMAL" },
+    ],
+};
