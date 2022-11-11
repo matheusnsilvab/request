a2eb7d1c6cbb011f05ccb6952257d5e2f5ecd7eb
Matheus Nunes <103690968+matheusnsilvab@users.noreply.github.com>
2022-11-11 13:37:24 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,51 +3,10 @@ const path = require('path')
 const app = express()
 const routes = require('./routes/routes')
 const connectToDb = require('./database/db')
-const providers = { users: [] }
-const { graphql } = require('graphql')
-const { ApolloServer, gql, UserInput } = require('apollo-server');
-const { Query } = require('mongoose')
-const resolvers = {
-    Query: {
-        users: () => users,
-        user: (_, { id }) => User.findById(id),
-    },
-    Mutation: {
-        createUser: (_, { data }) => User.create(data),
-        updateUser: (_, { id, data }) => User.findOneAndUpdate(id, data, { new: true }),
-        deleteUser: async (_, { id }) => !!(await User.findOneAndDelete(id)),
-    }
-};
-
-const typeDefs = gql`
-  type User {
-    _id: ID!
-    firstName: String!
-    lastName: String!
-    email: String!
-    active: Boolean!
-}
-  input UserInput {
-    firstName: String!
-    lastName: String!
-    email: String!
-    active: Boolean
-  }
-  type Query {
-  users: [User!]!
-  user(id: ID!): User!
- }
-
- type Mutation {
-    createUser(data: UserInput!): User!
-    updateUser(id: ID, data: UserInput!): User!
-    deleteUser(id: ID!): Boolean
- }
-`
-
+const { ApolloServer } = require("apollo-server");
+const graphql = require("./src/graphql");
 const server = new ApolloServer({
-    typeDefs,
-    resolvers
+  ...graphql,
 });
 
 connectToDb()
